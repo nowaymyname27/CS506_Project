1fbde56288b550c9442b4738d0bbb00f65ff2832
Andrey Mukhin <amukhin@bu.edu>
2025-04-29 09:48:08 -0400

    changed to take entire year of data

--- stock_extract/Stock-Extract.py
+++ stock_extract/Stock-Extract.py
@@ -5,6 +5,6 @@ from datetime import datetime
 import yfinance as yf
 
 tsla = yf.Ticker("TSLA")
-tsla_historical = tsla.history(start="2024-12-01", end="2024-12-31", interval="1d")
+tsla_historical = tsla.history(start="2024-01-01", end="2024-12-31", interval="1d")
 tsla_historical
-tsla_historical.to_csv('tsla_dec_daily.csv')
+tsla_historical.to_csv('tsla_daily_2024.csv')
